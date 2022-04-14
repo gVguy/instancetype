315c7eb94510a1b5083c887f875cc3ac0e28b6c8
Ivan Che <ivan.chepurin@gmail.com>
2022-04-14 14:11:37 +0300

    reproduce the instancetype problem

---
+++ src/components/ComponentA.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('hello')
+
+function setText(data: string) {
+  text.value = data
+}
+
+defineExpose({ setText })
+</script>
